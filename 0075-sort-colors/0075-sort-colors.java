f3545418fd54205b517da1fcf32be36edab5c657
Bhumika Awasthi <153513266+BHUMIKA-AWASTHI@users.noreply.github.com>
2025-02-09 18:29:24 +0530

    Time: 0 ms (100%), Space: 41.7 MB (91.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,4 +1,11 @@
 class Solution {
+
+    public void swap(int[] arr, int mid, int low){  // swap function to reduce the space complexity
+        int temp= arr[mid];
+        arr[mid]= arr[low];
+        arr[low]= temp;
+
+    }
     public void sortColors(int[] arr) {
         int n = arr.length;
         //3 pointer approach --> low, mid,
@@ -7,16 +14,12 @@ class Solution {
         int mid =0, high=n-1, low=0;
         while(mid<=high){
             if(arr[mid]==0){
-                int temp= arr[mid];
-                arr[mid]= arr[low];
-                arr[low]= temp;
+                swap(arr,mid,low);
                 low++; mid++;
             }
             else if(arr[mid]==1) mid++;
             else { //arr[mid]==2;
-                    int temp= arr[mid];
-                    arr[mid]= arr[high];
-                    arr[high]= temp;
+                    swap(arr,mid,high);
                     high--;
             }
         }
